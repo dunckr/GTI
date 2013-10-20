d7a30108048c628f40a5d4a44e2668f99b2c6d72
duncky <d@duncky.com>
2013-10-20 16:34:45 +0100

    wip

--- gti.js
+++ gti.js
@@ -1,14 +1,38 @@
 #!/usr/bin/env node
 
-// process = require('process')
-chalk = require('chalk')
+chalk = require('chalk');
 
 var i = 0;  // dots counter
-setInterval(function() {
-  process.stdout.clearLine();  // clear current text
-  process.stdout.cursorTo(0);  // move cursor to beginning of line
-  i = (i + 1) % 4;
-  var dots = new Array(i + 1).join(".");
-  process.stdout.write("Waiting" + dots);  // write text
-}, 300);
 
+
+setInterval(function() {
+  process.stdout.clearLine();
+  process.stdout.cursorTo(i);
+  process.stdout.write(drawCar());
+  // process.stdout.write('one')
+  i++;
+}, 100);
+
+
+function drawCar() {
+  return "   ,---------------.\n" +
+        "  /  /``````|``````\\\\\n" +
+        " /  /_______|_______\\\\________\n" +
+        "|]      GTI |'       |        |]\n" +
+        "=  .-:-.    |________|  .-:-.  =\n" +
+        " `  -+-  --------------  -+-  '\n" +
+        "   '-:-'                '-:-' \n ";
+}
+
+// line_at(x, "   ,---------------.");
+// line_at(x, "  /  /``````|``````\\\\");
+// line_at(x, " /  /_______|_______\\\\________");
+// line_at(x, "|]      GTI |'       |        |]");
+// if (x % 2) {
+// line_at(x, "=  .-:-.    |________|  .-:-.  =");
+// line_at(x, " `  -+-  --------------  -+-  '");
+// line_at(x, "   '-:-'                '-:-'  ");
+// } else {
+// line_at(x, "=  .:-:.    |________|  .:-:.  =");
+// line_at(x, " `   X   --------------   X   '");
+// line_at(x, "   ':-:'                ':-:'  ");
